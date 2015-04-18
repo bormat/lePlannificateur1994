54a7a992deb9ea59a8bc4546e010b7477b42fb21
bormat <i4wfxmavg>
2015-04-18 20:11:58 +0200

    version buggué mais il ya de l'orage donc commit

--- js/serialisation.js
+++ js/serialisation.js
@@ -8,6 +8,7 @@
 		if(!(theMainObject instanceof Object)){
 			throw "veuillez passer un objet et non un type primitif"
 		}
+		var tab=[];
 		function addSlashes(ch){
 			if (typeof ch == "string"){
 				ch = ch.replace(/\\/g,"\\\\")
@@ -16,7 +17,41 @@
 			}
 			return ch
 		}
-		var tab=[];
+		
+		//pseudo classe		
+		var Reference = {
+			tabDejaLier:[],
+			calculerTabNbSupp : function(){
+				var tabNbSupp = Reference.tabNbSupp = [0];
+				for(var i=1; i<Reference.tabDejaLier.length; i++){
+					tabNbSupp[i] = tabNbSupp[i - 1]
+					if (!Reference.tabDejaLier[i]){
+						tabNbSupp[i]++;
+					}
+				}
+			},
+			//constructeur
+			lier:function(ref,dejalier){
+					var obj = Object.create(Reference.prototype);
+					obj.ref = (ref != undefined) ? ref : tab.length ;
+					Reference.tabDejaLier[ref]=Boolean(dejalier);
+					if (isNaN(obj.ref)){
+						throw "erreur";
+					}
+					return obj;
+			},
+			prototype:{			
+				toString:function(){
+					if ( Reference.tabDejaLier[this.ref]){
+						var i = this.ref - Reference.tabNbSupp[this.ref];
+						return "'tab["+i+"]'";
+					}else{
+						return tab[this.ref].join("")
+					}
+				}
+			}	
+		}
+		
 		function genererTableauDeChaine(obj){
 			if(obj.serializationName){
 				obj.serializationName = obj.serializationName;// ceci n'est pas inutile __proto__ vers objet
@@ -32,10 +67,11 @@
 					if(typeof obj[i] == "number" || obj[i] instanceof Function){
 							chaine.push(obj[i]);
 					}else if ( obj[i] instanceof Object){
-						if (obj[i].myid == undefined){
+						var dejaLier = obj[i].myid != undefined;
+						chaine.push(Reference.lier(obj[i].myid, dejaLier));
+						if (!dejaLier){
 							genererTableauDeChaine(obj[i]);
 						}
-						chaine.push("'tab[",obj[i].myid,"]'");
 					}else{
 						chaine.push("'",addSlashes(obj[i]),"'");
 					}					
@@ -43,7 +79,7 @@
 				}
 			}
 			chaine.last("}");//que c'est beau last pour écraser les virgules
-			tab[obj.myid] = chaine.join("");
+			tab[obj.myid] = chaine;
 		}
 		function deleteMyid(obj){
 			if(obj && obj.myid != undefined){
@@ -54,11 +90,27 @@
 					}
 				}
 			}
-		}		
+		}
+
 		genererTableauDeChaine(theMainObject);
 		deleteMyid(theMainObject);	
-		
-		return "[" + tab.join(",\n") +"]"
+		Reference.calculerTabNbSupp();		
+		for (var i in tab){
+			tab[i] = tab[i].join("");
+		}
+		tab.toString=function(){
+			var toReturn = new Tab;
+			toReturn.push("[");
+			for (var i = 0; i<tab.length ;i++){
+				if( i==0 || Reference.tabDejaLier[i] ){
+					toReturn.push(tab[i],",");
+				}
+			}
+			toReturn.last("]")
+			return toReturn.join("");
+		}
+	
+		return ""+tab;
 	}
 
 
@@ -71,15 +123,21 @@
 		//function principal
 		function parse(chaine){
 			eval("tab="+chaine);
-			genererHeritage2()
-			for( var i in tab){ 
-				var obj=tab[i];
-				for( var prop in obj){				
-					if (contientUneRef(obj,prop)){
-						eval ("obj[prop]="+ obj[prop]);
+			tab[0] = genererHeritage2(tab[0])
+			function relierRef(obj){
+				obj.relier=true;
+				for( var prop in obj){
+					if(obj.hasOwnProperty(prop)){
+						if (contientUneRef(obj,prop)){
+							eval ("obj[prop]="+ obj[prop]);
+						}
+						if( obj[prop] instanceof Object && !obj[prop].relier){
+							relierRef(obj[prop])
+						}
 					}
 				}
 			}
+			relierRef(tab[0]);
 			nettoyage();
 			remettreLengthAuFauxTableaux();
 		}
@@ -88,23 +146,30 @@
 			return  prop && obj[prop].indexOf && obj[prop].indexOf("tab[") != -1 
 		}
 		
-		function genererHeritage2(obj){
-			for( var i in tab){ 
-				var theClass = window[tab[i].serializationName] || {};
-				theClass.prototype = theClass.prototype || {};
-				var obj = (theClass == Array) ? [] : Object.create(theClass.prototype) // bonne classe
-				// on remmet les infos
-				for (var j in tab[i]){
-					obj[j] = tab[i][j];
-				}
-				tab[i] = obj 
-			}
-		}	
+		function genererHeritage2(unObj){
+			if (unObj.heritageFait || !(unObj instanceof Object) ){
+				return unObj;
+			}	
+			unObj.heritageFait=true;
+			var theClass = window[unObj.serializationName] || {};
+			theClass.prototype = theClass.prototype || {};
+	
+			var newObj = (theClass == Array) ? [] : Object.create(theClass.prototype) // bonne classe
+			// on remmet les infos
+			for (var i in unObj){
+				newObj[i] = unObj[i];
+				newObj[i] = genererHeritage2(newObj[i]);
+			}				
+			return newObj;
+		}
 	
 		function nettoyage(){
 			for(var i in tab){
 				delete tab[i].serializationName;
+				delete tab[i].heritageFait;
+				delete tab[i].relier;
 			}
+
 		}
 		//un faux tableau est un objet héritant de Array mais dont la length n'est du coup plus automatique
 		function remettreLengthAuFauxTableaux(){
